0d1f10d22541662b9e260f97626ec74fdb4cd0b7
Brian Anderson <brian.briananderson@gmail.com>
2025-05-15 09:36:25 -0500

    merge

--- rerank.py
+++ rerank.py
@@ -54,3 +54,31 @@ def rerank_options(
     ranked_results.sort(key=lambda x: x[1], reverse=True)
     # Return the five best.
     return ranked_results[:k]
+
+
+async def rerank_options_async(
+    options: list[dict], query: str, k: int = 5, model_name: str = "bge"
+) -> list[tuple]:
+    """
+    Reranking magic.
+    """
+    ranker = Reranker(**rankers[model_name], verbose=False)
+    if ranker is None:
+        raise ValueError(
+            f"Ranker {model_name} not found. Please check the model name and try again."
+        )
+    ranked_results: list[tuple] = []
+    for option in options:
+        course = option["course_title"]
+        TOC = option["course_description"]
+        ranked = await ranker.rank_async(query=query, docs=[TOC])
+        # Different models return different objects (RankedResults or Result)
+        try:  # See if it's a RankedResults object
+            score = ranked.results[0].score
+        except:  # If not, it's a Result object
+            score = ranked.score
+        ranked_results.append((course, score))
+    # sort ranked_results by highest score
+    ranked_results.sort(key=lambda x: x[1], reverse=True)
+    # Return the five best.
+    return ranked_results[:k]
